86ecaa97f8172b3f814646705ed2abc8a8cc35b7
mcadr98 <168329040+mcadr98@users.noreply.github.com>
2026-01-05 09:03:20 -0600

    Update ejercicio02.js

--- ejercicio02.js
+++ ejercicio02.js
@@ -3,6 +3,10 @@
 
 //define tu función aquí
 
+function obtenerPI() {
+    const PI = 3.1416;
+    return PI;
+}
 
 // No modificar
 // Exporta la función para que pueda ser utilizada en otros archivos
